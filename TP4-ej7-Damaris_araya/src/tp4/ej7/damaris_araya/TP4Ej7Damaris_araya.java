4c1eed5279935e4f1a96016765aa38ffce682db2
damarisrobloxiana-dot <damarisrobloxiana@gmail.com>
2026-05-11 10:57:29 -0300

    Add files via upload

---
+++ TP4-ej7-Damaris_araya/src/tp4/ej7/damaris_araya/TP4Ej7Damaris_araya.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4.ej7.damaris_araya;
+
+
+public class TP4Ej7Damaris_araya {
+
+   
+    public static void main(String[] args) {
+          int[] numeros = new int[10];      
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = (int)(Math.random() * 100);
+        }
+        System.out.println("Numeros aleatorios:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        
+
+    }
+
+    
+
+}
+    
+   
+
